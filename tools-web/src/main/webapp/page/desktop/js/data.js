e0c4b442458052b0d7c192c51ec81d0ab06438ee
郑伟 <759047731@qq.com@7e3453b3-37a7-4250-ae78-7f0e680b2053>
2016-10-26 14:03:15 +0000

    提交

---
+++ tools-web/src/main/webapp/page/desktop/js/data.js
@@ -0,0 +1,362 @@
+﻿DATA={
+	menu:[{//一级菜单
+		menuid:'m001',
+		name:'主页',
+		code:'m001',
+		icon:'images/home.png'
+	},{
+		menuid:'m002',
+		name:'邮件',
+		code:'m002',
+		icon:'images/email.png'
+	},{
+		menuid:'m003',
+		name:'文件夹',
+		code:'m003',
+		icon:'images/portfolio.png'
+	},{
+		menuid:'m004',
+		name:'音乐',
+		code:'m004',
+		icon:'images/music.png'
+	},{
+		menuid:'m005',
+		name:'视频',
+		code:'m005',
+		icon:'images/video.png'
+	},{
+		menuid:'m006',
+		name:'备忘录',
+		code:'m006',
+		icon:'images/history.png'
+	},{
+		menuid:'m007',
+		name:'日历',
+		code:'m007',
+		icon:'images/calendar.png'
+	},{
+		menuid:'m008',
+		name:'RSS',
+		code:'m008',
+		icon:'images/rss.png'
+	}],
+	app:{//桌面1
+		'readGod':{
+			appid:'2534',
+			icon:'readGod.png',
+			name:'读览天下',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[{"+
+				"'appid':'8856',"+
+				"'icon':'sosomap.png',"+
+				"'name':'搜搜地图',"+
+				"'url':'http://www.jq-school.com/'"+
+			"},{"+
+				"appid:'8857',"+
+				"icon:'time.png',"+
+				"name:'时钟',"+
+				"url:'http://www.jq-school.com/'"+
+			"},{"+
+				"appid:'8858',"+
+				"icon:'jinshan.png',"+
+				"name:'金山快盘',"+
+				"url:'http://www.jq-school.com/'"+
+			"}]",
+			asc :1
+		},
+		'sosomap':{
+			appid:'42',
+			icon:'sosomap.png',
+			name:'搜搜地图',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[{"+
+				"'appid':'10010',"+
+				"'icon':'fastsearch.png',"+
+				"'name':'快递查询',"+
+				"'enname':'fastsearch',"+
+				"'url':'http://www.jq-school.com/',"+
+				"'asc' :1"+
+			"},{"+
+				"appid:'10011',"+
+				"icon:'doudizhu.png',"+
+				"enname:'doudizhu',"+
+				"name:'欢乐斗地主',"+
+				"url:'http://www.jq-school.com/',"+
+				"asc :2"+
+			"}]",
+			asc :2
+		},
+		'jinshan':{
+			appid:'8992',
+			icon:'jinshan.png',
+			name:'金山快盘',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'douban':{
+			appid:'3402',
+			icon:'douban.png',
+			name:'豆瓣电台',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'Pixlr':{
+			appid:'64',
+			icon:'pixlr.png',
+			name:'Pixlr',
+			url:'msg.html',
+			sonMenu:"[]",
+			asc:5
+		},
+		'weather':{
+			appid:'18',
+			icon:'5.png',
+			name:'天气',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc:6
+		},
+		'Clock':{
+			appid:'20',
+			icon:'time.png',
+			name:'时钟',
+			url:'http://www.jq-school.com/',
+			asc:7
+		},
+		'yule':{
+			appid:'514',
+			icon:'folder_o.png',
+			name:'娱乐',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc:8
+		},
+		'mangguo':{
+			appid:'3401',
+			icon:'mangguo.png',
+			name:'芒果旅游',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'tuanmap':{
+			appid:'2527',
+			icon:'tuanmap.png',
+			name:'团购地图',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'fastsearch':{
+			appid:'3693',
+			icon:'fastsearch.png',
+			name:'快递查询',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'bianqian':{
+			appid:'10',
+			icon:'bianqian.png',
+			name:'便签',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'wangdesk':{
+			appid:'13',
+			icon:'wangdesk.png',
+			name:'网络硬盘',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'friend':{
+			appid:'21',
+			icon:'friend.png',
+			name:'奥尔网',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'friendnear':{
+			appid:'31',
+			icon:'friendnear.png',
+			name:'好友近况',
+			url:'http://www.jq-school.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'friendgroup':{
+			appid:'32',
+			icon:'friendgroup.png',
+			name:'好友管理',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'kaikai':{
+			appid:'2250',
+			icon:'kaikai.png',
+			name:'开开',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'kxjy':{
+			appid:'2535',
+			icon:'kxjy.png',
+			name:'奥尔交友',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'qidianzhongwen':{
+			appid:'2528',
+			icon:'qidianzhongwen.png',
+			name:'起点中文',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'qqread':{
+			appid:'45',
+			icon:'qqread.png',
+			name:'奥尔阅读',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'xiami':{
+			appid:'2526',
+			icon:'xiami.png',
+			name:'虾米电台',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'musicbox':{
+			appid:'56',
+			icon:'musicbox.png',
+			name:'音乐盒子',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'vadio':{
+			appid:'15',
+			icon:'vadio.png',
+			name:'奥尔视频',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'leshi':{
+			appid:'3148',
+			icon:'leshi.png',
+			name:'乐视网',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :6
+		},
+		'doudizhi':{
+			appid:'48',
+			icon:'doudizhu.png',
+			name:'欢乐斗地主',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'3366':{
+			appid:'49',
+			icon:'3366.png',
+			name:'3366',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'qqbaby':{
+			appid:'26',
+			icon:'qqbaby.png',
+			name:'QQ宝贝',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'game':{
+			appid:'1000',
+			icon:'game.png',
+			name:'便签',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :4
+		}
+	},
+	sApp:{//侧边栏应用
+		'appmarket':{
+			appid:'1',
+			icon:'appmarket.png',
+			name:'应用市场',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :1
+		},
+		'qq':{
+			appid:'2',
+			icon:'big.png',
+			name:'QQ',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :2
+		},
+		'weibo':{
+			appid:'3',
+			icon:'weibo.png',
+			name:'微博',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :3
+		},
+		'mail':{
+			appid:'4',
+			icon:'mail.png',			
+			name:'邮箱',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :4
+		},
+		'zone':{
+			appid:'5',
+			icon:'zone.png',
+			name:'空间',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :5
+		},
+		'internet':{
+			appid:'6',
+			icon:'internet.png',
+			name:'浏览网页',
+			url:'http://www.juheweb.com/',
+			sonMenu:"[]",
+			asc :6
+		}
+	
+	}
+};
+ops = {//向桌面添加应用
+	Icon1:['readGod','sosomap','jinshan','douban','Pixlr','yule','weather','Clock'],
+	Icon2:['mangguo','tuanmap','fastsearch','bianqian','wangdesk'],
+	Icon3:['friend','friendnear','friendgroup','kaikai','kxjy'],
+	Icon4:['qidianzhongwen','qqread','xiami','musicbox','vadio','leshi'],
+	Icon5:['doudizhi',	'3366',	'qqbaby','game'],
+	Icon6:['3366','friendnear','yule','kaikai','Clock'],
+	Icon7:['friend','sosomap','xiami','game','kxjy'],
+	Icon8:['friend','mangguo']
+}
+//初始化左边快捷菜单
+var leftMenu = new Array(['appmarket','qq','weibo','mail','internet','zone']);
+
+
